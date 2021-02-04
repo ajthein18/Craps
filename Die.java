c8d9cb4fe7d08cc382ef8fa9d6a7246a94726529
Andrew Thein <athein22@students.polytechnic.org>
2021-02-04 14:23:32 -0800

    Complete Game class. Added print statements and press enter to roll dice

--- Die.java
+++ Die.java
@@ -29,4 +29,5 @@ public class Die
         return roll;
     }
     
+    
 }
--- Game.java
+++ Game.java
@@ -12,36 +12,44 @@ public class Game
     {
         Die d1 = new Die();
         Die d2 = new Die();
+        Scanner in = new Scanner(System.in);
         
-        
+        System.out.println("Press [enter] to roll the dice");
+        in.nextLine(); // waits for user to press enter
         int total = d1.roll() + d2.roll();
+        System.out.print("You rolled a " + total + ". ");
         if (total == 7 || total == 11)
         {
-            // won the game
+            // wins game
+            System.out.println( "You win!");
         }
         else if (total == 2 || total == 3 || total == 12)
         {
             // lost the game
+            System.out.println("Sadly, you lose :(");
         }
         
         else //haven't won or lost game
         {
             int point = total;
+            System.out.println(point + " is your point. Press [enter] to roll again.");
+            in.nextLine(); // waits for user to press enter
             total = d1.roll() + d2.roll();
             while (total != 7 && total != point)
             {
-                // haven't won or lost yet
+                System.out.println("You rolled a " + total + ". Press [enter] to roll again.");
+                in.nextLine(); // waits for user to press enter
                 total = d1.roll() + d2.roll();
             }
             
             if (total == point)
             {
-                // won game
+                System.out.println("You rolled your point. You win!");
             }
             
             else 
             {
-                // lost game
+                System.out.println("You rolled a 7. You lose :(");
             }
         }
     }
